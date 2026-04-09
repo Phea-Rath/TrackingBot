44f7c13c52629b20559385b155944dd61069245e
Tep Phearath <160566976+Phea-Rath@users.noreply.github.com>
2026-04-09 13:41:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def handle_track(message):
             sub_text = item.get('TrackEnName', '') # បង្ហាញអង់គ្លេសពីក្រោមតូចៗ
         
             response += f"{icon} **{date_str}\n"
-            response += f"┃  `**{kh_status}`\n" if kh_status else ""
+            response += f"┃  **{kh_status}\n" if kh_status else ""
             response += f"┃  `{sub_text}`\n" if sub_text else ""
             response += f"{connector}\n"
             
